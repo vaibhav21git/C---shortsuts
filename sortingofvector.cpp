96ddbe0d2db0fd46d1ebbaf67ca796d9740b6509
vaibhav <vaibhavsachdeva2000@gmail.com>
2022-08-14 15:42:35 +0530

    directions tricks

--- sortingofvector.cpp
+++ sortingofvector.cpp
@@ -1,5 +1,15 @@
-// Sorting the edges array based on dist of each pair of node. 
-        sort(edges.begin(),edges.end(),[](vector<int>&a, vector<int>&b){
+// Sorting the edges array based on dist of each pair of node.
+sort(edges.begin(), edges.end(), [](vector<int> &a, vector<int> &b)
+     {
             if(a[2]<b[2]) return true;
-            return false;
-        });
+            return false; });
+
+// to move in four direction
+vector<int> dir = {0, 1, 0, -1, 0};
+
+// run a for loop
+// for(int i = 0;i<4;i++)
+// {
+//    i , i+1
+// this will give u 4 directions
+// }
